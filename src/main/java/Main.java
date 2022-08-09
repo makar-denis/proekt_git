cffa7017ca1f6ea15cafe5412c903bf3651035c4
Денис <makar_denis@tut.by>
2022-08-09 23:22:40 +0300

    сделал ветку den  и добавил 3 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет");
         System.out.println("2 commit");
+        System.out.println("3 commit");
     }
 }
